f6a6ea013db1dbfbf2918810d41f4fd87f401ae0
maximilien <ro.maximilien@gmail.com>
2025-05-13 11:34:23 +0200

    first commit

---
+++ src/apis/expense.api.js
@@ -0,0 +1,27 @@
+import { BASE_URL } from "../utils/url";
+
+export async function addAnExpense(values, id) {
+  try {
+    const response = await fetch(`${BASE_URL}/expenses`, {
+      method: "POST",
+      body: JSON.stringify({ ...values, user: id }),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    const addedExpense = await response.json();
+    return addedExpense;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getExpensesByUser(id) {
+  try {
+    const response = await fetch(`${BASE_URL}/expenses/${id}`);
+    const allExpenses = await response.json();
+    return allExpenses;
+  } catch (error) {
+    console.log(error);
+  }
+}
